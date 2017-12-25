cf0c37683646d8728b6b4759342db3c7df0528f5
Ivan Vučica <ivan@vucica.net>
2017-12-25 14:13:21 +0000

    day3: Do not fail immediately after a test fails.

--- day3/day3_test.sh
+++ day3/day3_test.sh
@@ -15,25 +15,18 @@ if [[ ${_out} != 0 ]] ; then
 	_failed=1	
 fi
 
-if [[ ${_failed} == 1 ]] ; then
-	exit 1
-fi
-
-_failed=0
 _out="$("${BIN}" 9)"
 if [[ ${_out} != 2 ]] ; then
 	echo "for input 9, expected = 2, got = ${_out}"
 	_failed=1	
 fi
 
-_failed=0
 _out="$("${BIN}" 10)"
 if [[ ${_out} != 3 ]] ; then
 	echo "for input 10, expected = 3, got = ${_out}"
 	_failed=1	
 fi
 
-_failed=0
 _out="$("${BIN}" 11)"
 if [[ ${_out} != 2 ]] ; then
 	echo "for input 11, expected = 2, got = ${_out}"
@@ -43,21 +36,18 @@ fi
 
 
 
-_failed=0
 _out="$("${BIN}" 12)"
 if [[ ${_out} != 3 ]] ; then
 	echo "for input 12, expected = 3, got = ${_out}"
 	_failed=1	
 fi
 
-_failed=0
 _out="$("${BIN}" 23)"
 if [[ ${_out} != 2 ]] ; then
 	echo "for input 23, expected = 2, got = ${_out}"
 	_failed=1	
 fi
 
-_failed=0
 _out="$("${BIN}" 1024)"
 if [[ ${_out} != 31 ]] ; then
 	echo "for input 1024, expected = 31, got = ${_out}"
